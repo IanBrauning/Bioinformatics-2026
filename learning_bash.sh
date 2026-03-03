0c495c5e6bf98cfad2d533002b74cfbc0d465fdf
Ian Brauning <ian.c.brauning@wmich.edu>
2026-03-03 15:14:46 -0500

    Add files via upload
    Assignment 2

---
+++ learning_bash.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+greeting="Hello"
+name="Ian"
+
+echo "$greeting $name!"
+echo "This script is helping you learn bash."
+
+mkdir ~/Desktop/bioinformatics
+mkdir ~/Desktop/bioinformatics/bash_scripts
+
+mv ~/Desktop/bash_template.txt ~/Desktop/bioinformatics/bash_scripts/bash_template.txt
+
+mv ~/Desktop/bioinformatics/bash_scripts/bash_template.txt ~/Desktop/bioinformatics/bash_scripts/bash_template.sh
